018b419e90f32c5138a762c811cf6338ddef42e0
Gulshan Gaur <gaur.gulshan@gmail.com>
2025-12-19 13:49:31 +0530

    site deploy

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 (function ($) {
     "use strict";
-    
-    // Dropdown on mouse hover
+
+    // Dropdown on hover
     $(document).ready(function () {
         function toggleNavbarMethod() {
             if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
+                $('.navbar .dropdown')
+                    .on('mouseover', function () {
+                        $('.dropdown-toggle', this).trigger('click');
+                    })
+                    .on('mouseout', function () {
+                        $('.dropdown-toggle', this).trigger('click').blur();
+                    });
             } else {
                 $('.navbar .dropdown').off('mouseover').off('mouseout');
             }
@@ -17,9 +19,8 @@
         toggleNavbarMethod();
         $(window).resize(toggleNavbarMethod);
     });
-    
-    
-    // Back to top button
+
+    // Back to top
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.back-to-top').fadeIn('slow');
@@ -27,30 +28,41 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
+
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
-    });   
-})(jQuery);
-$("#header").load("components/header.html", function () {
-        $(window).trigger('scroll'); // 🔥 important
     });
 
+    // Testimonial Carousel
+    $(document).ready(function () {
+        $('#testimonialCarousel').carousel({
+            interval: 4000,
+            ride: 'carousel',
+            wrap: true
+        });
+    });
+
+    // Load Header & Footer
+    $("#header").load("components/header.html", function () {
+        $(window).trigger('scroll');
+    });
     $("#footer").load("components/footer.html");
-    $('#contactForm').on('submit', function (e) {
-    let isValid = true;
 
-    $(this).find('[required]').each(function () {
-        if (!$(this).val().trim()) {
-            isValid = false;
-            $(this).next('small').removeClass('d-none');
-        } else {
-            $(this).next('small').addClass('d-none');
-        }
+    // Contact Form Validation
+    $('#contactForm').on('submit', function (e) {
+        let isValid = true;
+
+        $(this).find('[required]').each(function () {
+            if (!$(this).val().trim()) {
+                isValid = false;
+                $(this).next('small').removeClass('d-none');
+            } else {
+                $(this).next('small').addClass('d-none');
+            }
+        });
+
+        if (!isValid) e.preventDefault();
     });
 
-    if (!isValid) {
-        e.preventDefault(); // form submit stop
-    }
-});
-
+})(jQuery);
